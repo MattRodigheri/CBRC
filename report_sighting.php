d9e5b019ec3b0a245062a760ea800ebd5ba3e590
MattRodigheri <mrodigheri@gmail.com>
2016-03-11 15:48:44 -0800

    email on form submission

--- report_sighting.php
+++ report_sighting.php
@@ -1,65 +1,20 @@
 <?php
+// Here we get all the information from the fields sent over by the form.
 $msg = "SIGHTING REPORT\n\n";
 $msg .= "Species:    $_POST[species]\n";
 $msg .= "Date of Observation:    $_POST[date]\n";
 $msg .= "Location:       $_POST[location]\n";
 $msg .= "Sighting Description:              $_POST[description]\n";
-$msg .= "Name of Observer:       $_POST[name]\n\n";
+$msg .= "Name of Observer:       $_POST[name]\n";
+$msg .= "Email of Observer:       $_POST[email]\n\n";
 $to = "mrodighe@mail.ccsf.edu";
-$subject = "Form Feedback Test";
+$subject = "Sighting Report";
 mail($to, $subject, $msg);
+
+// if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // this line checks that we have a valid email address
+// mail($to, $subject, $msg); //This method sends the mail.
+echo "Your email was sent!"; // success message
+// }else{
+// echo "Invalid Email, please provide an correct email.";
+// }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-<meta charset="utf-8">
-
-<title>Sighting Reported</title>
-
-<style>
-
-body {background-color: blue;}
-
-#contents {
-
-	margin: 0px auto; padding: 15px;
-        text-align: left; /* Counteract to IE/Win Hack */
-        width: 630px; background-color: lightblue;}
-
-</style>
-
-</head>
-
-<body>
-
-<section>
-
-<br><br>
-
-<div id="contents">
-
-<h3>Thank you, your sighting has been sent for approval</h3>
-
-<!-- <p><b>Your Name: </b>
-<?php echo "$_POST[name]"; ?>
-<br><br>
-
-<b>Your Car Model: </b>
-<?php echo "$_POST[car]"; ?>
-<br><br>
-
-<b>Your Favorite Color: </b>
-<?php echo "$_POST[color]"; ?>
-<br><br>
-
-<b>Your Message:</b>
-<?php echo "$_POST[message]"; ?>
-</p>
-
-</div> -->
-
-</body>
-</html>
